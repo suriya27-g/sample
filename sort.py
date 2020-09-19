8dca7881fedb17e4c9ae90b6684ec2e9aaa24214
Suriya Prakash <suriya27.bca@gmail.com>
2020-09-19 12:40:29 +0530

    Bouble sort code added

--- sort.py
+++ sort.py
@@ -1,2 +1,18 @@
 print('hellow')
 print('Welcome to git')
+
+#Bouble sort code
+def sort(list):
+
+    for i in range(len(list)-1,0,-1):
+        for j in range(i):
+
+            if list[j] > list[j+1]:
+                list[j],list[j+1]=list[j+1],list[j]
+
+
+list=[4,2,7,1,5]
+
+sort(list)
+
+print(list)
